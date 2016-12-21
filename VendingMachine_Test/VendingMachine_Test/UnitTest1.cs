6a08bb9b028133dea3332ae085394408d9107e72
brad_rust <brad.rust@uky.edu>
2016-12-21 03:57:33 -0500

    test whenNoCoinsHaveBeenInserted_displayReadsInsertCoin passes trivial implementation

---
+++ VendingMachine_Test/VendingMachine_Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using VendingMachine_Kata;
+
+namespace VendingMachine_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void whenNoCoinsHaveBeenInserted_displayReadsInsertCoin()
+        {
+            VendingMachine vm = new VendingMachine();
+            Assert.AreEqual(vm.display, "INSERT COIN");
+        }
+    }
+}
